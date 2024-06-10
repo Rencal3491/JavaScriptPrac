502f7317560551e9b1d6aeb1f7caf378f76c88a7
MJMM <jmj3491329@naver.com>
2024-06-10 15:02:41 +0900

    scope2

--- UsefulJavaScript/scope2.js
+++ UsefulJavaScript/scope2.js
@@ -14,3 +14,21 @@ myFunction();
 console.log('global scope: ' + value)
 console.log('-----------------')
 
+// Hoisting
+// 자바스크립트에서 아직 선언되지 않은 함수/변수를 끌어올려
+// 사용할 수 있는 자바스크립트의 작동 방식
+
+myFunctionHoisting();
+
+function myFunctionHoisting() {
+    console.log('Hello world!');
+}
+
+console.log(number);
+var number = 2;
+
+/*
+var number;
+cosole.log(number);
+number = 2;
+*/
